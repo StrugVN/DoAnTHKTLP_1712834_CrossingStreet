6e1490fd97dacf75c0f95ebc3ca9038c77b169f2
StrugVN <37741395+StrugVN@users.noreply.github.com>
2018-05-03 09:18:27 +0700

    :))

--- DoAnTHKTLP_1712834_CrossingStreet/Source.cpp
+++ DoAnTHKTLP_1712834_CrossingStreet/Source.cpp
@@ -98,10 +98,10 @@ void LoadResources() {
 	CR[1].loadFromFile("Res/CarR2.png");
 	CR[2].loadFromFile("Res/CarR3.png");
 	CR[3].loadFromFile("Res/CarR4.png");
-	CR[4].loadFromFile("Res/CarR5.png");
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 4; i++) {
 		C_R[i].setTexture(CR[i]);
 		C_R[i].setOrigin(Vector2f(C_R[i].getTexture()->getSize().x * 0.5, C_R[i].getTexture()->getSize().y*0.5));
+		C_R[i].scale(0.75f, 0.75f);
 	}
 
 	CL[0].loadFromFile("Res/CarL1.png");
@@ -111,6 +111,7 @@ void LoadResources() {
 	for (int i = 0; i < 4; i++) {
 		C_L[i].setTexture(CL[i]);
 		C_L[i].setOrigin(Vector2f(C_L[i].getTexture()->getSize().x * 0.5, C_L[i].getTexture()->getSize().y*0.5));
+		C_L[i].scale(0.75f, 0.75f);
 	}
 }
 
@@ -126,11 +127,77 @@ Point Player;
 bool endgame = false;
 Point *finished = NULL; int fn = 0;
 
+Point *R1 = NULL; int cr1 = 0;
+Point *R2 = NULL; int cr2 = 0;
+Point *L3 = NULL; int cl3 = 0;
+Point *L4 = NULL; int cl4 = 0;
+
+bool Collision(Sprite a, Sprite b) {
+	return a.getGlobalBounds().intersects(b.getGlobalBounds());
+}
+
+int random(int a, int b) {
+	if (a >= b)
+		return a;
+
+	return rand() % (b - a + 1) + a;
+}
+
+void PushFn(Point *&A, int &n) {
+	if (n == 0) {
+		A = new Point[1];
+	}
+	else {
+		Point* temp = new Point[n + 1];
+		for (int i = 0; i < n; i++)
+			temp[i] = A[i];
+		delete[] A;
+		A = temp;
+	}
+	n++;
+}
+
+void addCars(Point *&A, int &n, char c, int yrow, int count) {
+	float mid = 1000.0f / (count+1);
+	float CW = 100;
+	float pos = CW;
+	while (pos + mid < 1024 - CW) {
+		PushFn(A, n);
+		A[n - 1].x = random(pos, pos + mid-CW);
+		A[n - 1].y = yrow;
+		if (c == 'L' || c == 'l')
+			A[n - 1].p = C_L[random(0, 3)];
+		else
+			A[n - 1].p = C_R[random(0, 3)];
+		A[n - 1].p.setPosition(A[n - 1].x, A[n - 1].y);
+		pos += mid + CW;
+		cout << "Car added at " << A[n - 1].x << " " << A[n - 1].y << endl;
+	}
+}
+
 void Setup() {
 	Player.x = 500;
 	Player.y = 720;
 	Player.p = Sp[0];
 	Player.p.setPosition(Player.x, Player.y);
+
+	srand(time(NULL));
+
+	addCars(R1, cr1, 'R', 615, random(2,4));
+	addCars(R2, cr2, 'R', 460, random(2,4));
+	addCars(L3, cl3, 'L', 320, random(2,4));
+	addCars(L4, cl4, 'L', 200, random(2,4));
+}
+
+void DrawCars() {
+	for (int i = 0; i < cr1; i++)
+		window.draw(R1[i].p);
+	for (int i = 0; i < cr2; i++)
+		window.draw(R2[i].p);
+	for (int i = 0; i < cl3; i++)
+		window.draw(L3[i].p);
+	for (int i = 0; i < cl4; i++)
+		window.draw(L4[i].p);
 }
 
 void Move() {
@@ -197,25 +264,11 @@ void Move() {
 	}
 }
 
-void PushFn() {
-	if (fn==0) {
-		finished = new Point[1];
-	}
-	else {
-		Point* temp = new Point[fn + 1];
-		for (int i = 0; i < fn; i++)
-			temp[i] = finished[i];
-		delete[] finished;
-		finished = temp;
-	}
-}
-
 void CheckWin() {
 	if (Player.y <= 95) {
-		PushFn();
-		finished[fn] = Player;
-		finished[fn].p.setPosition(finished[fn].x, finished[fn].y);
-		fn++;
+		PushFn(finished,fn);
+		finished[fn-1] = Player;
+		finished[fn-1].p.setPosition(finished[fn-1].x, finished[fn-1].y);
 
 		Player.x = 500;
 		Player.y = 720;
@@ -223,9 +276,7 @@ void CheckWin() {
 	}
 }
 
-bool Collision(Sprite a, Sprite b) {
-	return a.getGlobalBounds().intersects(b.getGlobalBounds());
-}
+
 
 void RunMenu() {
 	while (true) {
@@ -284,6 +335,7 @@ void Run() {
 		window.clear();
 		window.draw(BG);
 		window.draw(Player.p);
+		DrawCars();
 		for (int i = 0; i < fn; i++)
 			window.draw(finished[i].p);
 		window.display();
